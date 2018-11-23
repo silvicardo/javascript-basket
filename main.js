21e55ea4bdca4af95e61d0bcb379b00427f25539
silvicardo <silvicardo@gmail.com>
2018-11-23 12:56:23 +0100

    project functions and logic

--- main.js
+++ main.js
@@ -15,3 +15,68 @@ Una volta generato il “database”, il programma deve chiedere
 all’utente di inserire un Codice Giocatore e il programma
 restituisce le statistiche.
 */
+
+var databaseGiocatori = generaDatabaseGiocatori(10);
+
+//GESTISCI UI
+
+interrogaIlDatabasePerId(parseInt(prompt('Digita id Giocatore desiderato')));
+
+
+// FUNZIONI
+
+function interrogaIlDatabasePerId(id) {
+
+    //Da inserire eventuale validazione
+
+    var risultatoQuery = databaseContiene(id);
+    if (risultatoQuery == false) {
+      stampaASchermoErrore();
+    } else {
+      stampaASchermoGiocatoreDa(id);
+    }
+}
+
+function stampaASchermoErrore() {
+  alert('Nessun Giocatore trovato con questo id');
+}
+
+function stampaASchermoGiocatoreDa(id) {
+
+  //Utilizzare for in per stampare tutte le statistiche
+}
+
+function databaseContiene(id) {
+
+  var trovato = false;
+  var indiceRicerca = 0;
+  while (trovato == false || database[indiceRicerca] < database.length) {
+    for (var chiave in database[i]){
+      if (chiave == 'id' && database.includes(database[i][chiave])){
+        trovato = true;
+      }
+    }
+  }
+
+}
+
+function generaDatabaseGiocatori(nrGiocatori) {
+  var arrayGiocatori = [];
+  var arrayId = generaIdCasualiDifferentiPer(nrGiocatori);
+
+  for (var i = 0; i < arrayId.length; i++) {
+    arrayGiocatori[i] = generaNuovoOggettoGiocatoreRandomcon(arrayId[i]);
+  }
+
+  return arrayGiocatori;
+}
+
+function generaIdCasualiDifferentiPer(totaleId){
+
+
+}
+
+function generaNuovoOggettoGiocatoreRandomcon(id) {
+
+
+}
