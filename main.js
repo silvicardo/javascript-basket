eed28b102f34814e0c8d45ab5f3609a14c617e4c
silvicardo <silvicardo@gmail.com>
2018-11-23 11:15:27 +0100

    project files

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+Il software deve generare casualmente le statistiche di gioco di
+100 giocatori di basket per una giornata di campionato.
+In particolare vanno generate per ogni giocatore le seguenti
+informazioni, facendo attenzione che il numero generato abbia
+senso:
+- Codice Giocatore Univoco (formato da 3 lettere
+maiuscole casuali e 3 numeri)
+- Numero di punti fatti
+- Numero di rimbalzi
+- Falli
+- Percentuale di successo per tiri da 2 punti
+- Percentuale di successo per da 3 punti
+Una volta generato il “database”, il programma deve chiedere
+all’utente di inserire un Codice Giocatore e il programma
+restituisce le statistiche.
+*/
